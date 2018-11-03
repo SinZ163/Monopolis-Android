ea3599efa335f3e342d25c2a24043fbb9535b49e
Samuel Cattini-Schultz <syriiins@gmail.com>
2018-11-04 01:52:26 +1100

    Add game screen for things

---
+++ things/src/main/java/me/syrin/monopolis/GameActivity.kt
@@ -0,0 +1,12 @@
+package me.syrin.monopolis
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+
+class GameActivity : FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
