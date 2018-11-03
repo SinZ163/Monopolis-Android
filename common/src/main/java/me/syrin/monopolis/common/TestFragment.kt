bcb4953abd0fa3185184b5316cbb661bce9a1187
Trent Monahan <github@361zn.is>
2018-11-04 00:33:35 +1100

    Implemented two way JSON packets over WebSocket (#10)

--- common/src/main/java/me/syrin/monopolis/common/TestFragment.kt
+++ common/src/main/java/me/syrin/monopolis/common/TestFragment.kt
@@ -1,28 +1,43 @@
 package me.syrin.monopolis.common
 
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import io.reactivex.android.schedulers.AndroidSchedulers
+import kotlinx.android.synthetic.main.fragment_test.*
+import me.syrin.monopolis.common.network.ChatPacket
+import me.syrin.monopolis.common.network.EventBus
+import me.syrin.monopolis.common.network.WebSocket
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class TestFragment : Fragment() {
-
+    lateinit var ws: WebSocket
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        // TEMP: Wake up the connection
+        val ws = WebSocket.instance
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_test, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        send.setOnClickListener {
+            WebSocket.send(ChatPacket("I'm making a note here, huge success"))
+        }
+    }
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        EventBus.subscribe<ChatPacket>()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    dump.text = dump.text.toString() + "\n" +it.msg
+                    Log.i("TestFragment", "Did you see ${it.msg}")
+                }
     }
 
 
