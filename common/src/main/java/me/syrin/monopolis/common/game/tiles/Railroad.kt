00056add59ec17e5753e7d01e305ed8603396cdc
Samuel Cattini-Schultz <syriiins@gmail.com>
2018-11-04 21:27:37 +0800

    Fix utility, railroad, and estate inheritance header (#16)

--- common/src/main/java/me/syrin/monopolis/common/game/tiles/Railroad.kt
+++ common/src/main/java/me/syrin/monopolis/common/game/tiles/Railroad.kt
@@ -3,7 +3,7 @@ package me.syrin.monopolis.common.game.tiles
 import me.syrin.monopolis.common.game.Monopolis
 import me.syrin.monopolis.common.game.Player
 
-class Railroad(name: String) : Property(name, 200) {
+class Railroad(name: String) : Property(name, 200, PropertySet.Railroad) {
     override fun chargePlayer(game: Monopolis, player: Player) {
         // TODO: charge player
     }
