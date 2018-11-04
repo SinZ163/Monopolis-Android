06bd0df7a0ba70666332fcd84da244f1840e650e
Samuel Cattini-Schultz <syriiins@gmail.com>
2018-11-04 14:39:25 +1100

    Add generic dialog fragment to common and test implemtation to things

--- things/src/main/java/me/syrin/monopolis/GameActivity.kt
+++ things/src/main/java/me/syrin/monopolis/GameActivity.kt
@@ -2,11 +2,26 @@ package me.syrin.monopolis
 
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
+import kotlinx.android.synthetic.main.activity_game.*
+import me.syrin.monopolis.common.GenericMessageDialogFragment
 
 class GameActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        button_test.setOnClickListener {
+            displayGenericMessageDialog("test", "testererereerererewrer")
+        }
+    }
+
+    private fun displayGenericMessageDialog(title: String, description: String) {
+        val dialog = GenericMessageDialogFragment()
+        val bundle = Bundle()
+        bundle.putString("title", title)
+        bundle.putString("description", description)
+        dialog.arguments = bundle
+        dialog.show(supportFragmentManager, "test")
     }
 }
