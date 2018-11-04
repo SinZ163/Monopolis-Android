fb23adf94b0177497c7d52383638b22bb0038a35
Samuel Cattini-Schultz <syriiins@gmail.com>
2018-11-05 01:01:54 +1100

    Fix things crashing on pregame

--- things/src/main/java/me/syrin/monopolis/PreGameActivity.kt
+++ things/src/main/java/me/syrin/monopolis/PreGameActivity.kt
@@ -3,6 +3,7 @@ package me.syrin.monopolis
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
 import kotlinx.android.synthetic.main.activity_pre_game.*
+import me.syrin.monopolis.common.network.Monopolis
 import org.jetbrains.anko.startActivity
 
 class PreGameActivity : FragmentActivity() {
@@ -11,17 +12,8 @@ class PreGameActivity : FragmentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pre_game)
 
-        val name = intent.extras?.get("game_name") as String
+        game_name.text = Monopolis.lobby.value?.name
         val playerCount = intent.extras?.get("player_count") as Int
-
-        if (name != "") {
-            // game is being created
-            game_name.text = name
-        }
-        else {
-            // game is being joined
-
-        }
 
         button_start_game.setOnClickListener {
             startActivity<GameActivity>()
