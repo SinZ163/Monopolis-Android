32ff5082f19bf8e0713e2f392267c1d58b9685b1
Trent Monahan <github@361zn.is>
2018-11-07 15:10:31 +1100

    Added the ability for mobile to select board type (#27)
    Also fixed misc gameplay bugs
    also did refactor of BoardFragment preparing for runtime updates

--- mobile/src/main/java/me/syrin/monopolis/GameActivity.kt
+++ mobile/src/main/java/me/syrin/monopolis/GameActivity.kt
@@ -7,13 +7,26 @@ import me.syrin.monopolis.common.BoardFragment
 import me.syrin.monopolis.common.game.Monopolis
 
 class GameActivity : AppCompatActivity() {
-
+    lateinit var monopolis: Monopolis
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
-        val monopolis = Monopolis(this)
+        monopolis = Monopolis(this, listOf("Sam", "Trent"))
 
         (fragment2 as BoardFragment).initialiseBoard(monopolis)
+
+        text_view_temp.text = "${monopolis.players[0].name}: ${tempGetLocName(monopolis.players[0].location)}\n${monopolis.players[1].name}: ${tempGetLocName(monopolis.players[1].location)}"
+
+        button_test.setOnClickListener {
+            // do turn
+            monopolis.doTurn()
+            // update player status
+            text_view_temp.text = "${monopolis.players[0].name}: ${tempGetLocName(monopolis.players[0].location)}\n${monopolis.players[1].name}: ${tempGetLocName(monopolis.players[1].location)}\nRoll: ${monopolis.diceTotal()}"
+        }
+    }
+
+    private fun tempGetLocName(i: Int): String {
+        return monopolis.tiles[i].name
     }
 }
